063bb4e01e23a969ae5d2fe4cf3687240bb6c70b
Yejin <vgg789@naver.com>
2023-03-27 10:21:13 +0900

    [java10] JAVA_BASIC_05_ETC

--- JAVA_CODING_TEST/src/Main.java
+++ JAVA_CODING_TEST/src/Main.java
@@ -1,36 +1,36 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
+import java.util.Stack;
 
 public class Main {
-	
+
 	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in)); //숫자로도 받을 수 있음 
-		int[] student = new int[31]; // 1~30명 
-		int[] studentNo = new int[31]; // 1~30명 
-		int num = 0;
-		
-		for (int i=0; i<28; i++) {
-			student[Integer.parseInt(br.readLine())] = 1;
-		}
-		
-		for (int j=0; j<student.length; j++) {
-			if (student[j] != 1) {
-				studentNo[num] = j;
-				num++;
-			}
-		}
-		
-		Arrays.sort(studentNo);
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in)); // 숫자로도 받을 수 있음
+		int N = Integer.parseInt(br.readLine());
+		Stack<String> stack = new Stack<>();
 
-        for (int i = 0; i < studentNo.length; i++) {
-        	if (studentNo[i] != 0) {
-                System.out.println(studentNo[i]);
+		for (int i = 0; i < N; i++) {
+			String str = br.readLine();
+			for (int j = 0; j < str.length(); j++) {
+				if (str.charAt(j) == '(') {
+					stack.push("(");
+				} else {
+					if (stack.size() == 0) {
+						stack.push(")");
+						break;
+					} else {
+						stack.pop();
+					}
+				}
 			}
-        }
-		
-		
+			
+			if (stack.empty()) {
+				System.out.println("YES");
+			} else {
+				System.out.println("NO");
+			}
+			stack.clear();
+		}
 	}
-
 }
