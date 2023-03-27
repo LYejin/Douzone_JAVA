2545c792d0c1b5c40100ed8b5865e6487bddc936
Yejin <vgg789@naver.com>
2023-03-27 11:49:40 +0900

    [java10] Ex02_Array_Quiz

--- JAVA_BASIC_03_Array_Object/src/Ex02_Array_Quiz.java
+++ JAVA_BASIC_03_Array_Object/src/Ex02_Array_Quiz.java
@@ -22,6 +22,57 @@ public class Ex02_Array_Quiz {
 		}
 		
 		System.out.printf("max >> %d, min >> %d", max, min);
+		System.out.println();
+		System.out.println();
+		
+		/////////////////////////////////////////////////
+		
+		int[] numbers = new int[10];
+		//10개의 방의 값을 1 ~ 10까지로 초기화하고 출력하세요
+		
+		int num = 0;
+		
+		for (int i=1; i<11; i++) {
+			numbers[num] = i;
+			num++;
+		}
+		
+		// 출력 
+		for (int i=0; i<10; i++) {
+			System.out.print(numbers[i]);
+		}
+		System.out.println();
+		
+		//////////////////////////////////////////////////
+		
+		
+		//어느 학생의 기말고사 시험점수 (5과목)
+		int[] jumsu = {100,55,90,60,78};
+		int sum = 0;
+		float avg = 0f;
+				
+		//1. 총과목의 수
+		System.out.println();
+		System.out.println(jumsu.length + "과목");
+		
+		//2. 과목의 합
+		System.out.println();
+		for (int i=0; i<jumsu.length; i++) {
+			sum += jumsu[i];
+		}
+		System.out.println("과목의 합 : " + sum);
+		
+		//3. 과목의 평균
+		System.out.println();
+//		for (int i=0; i<jumsu.length; i++) {
+//			sum += jumsu[i];
+//		}
+		avg = (sum/jumsu.length);
+		System.err.println("평균 : " + avg);
+		
+		
+		//단 2,3문제는 하나의 for으로 해결
+		
 	}
 
 }
